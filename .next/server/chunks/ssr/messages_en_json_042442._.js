374392a1f0b93ddf86c5c32a4e0a3c2e4a7cccff
Togo <togo@hasrec.com>
2025-02-10 14:40:43 +0900

    removed several files

--- .next/server/chunks/ssr/messages_en_json_042442._.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-"[project]/messages/en.json (json)": ((__turbopack_context__) => {
-
-var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, t: __turbopack_require_real__ } = __turbopack_context__;
-{
-__turbopack_export_value__(JSON.parse("{\"Home\":{\"profileNotFound\":\"Unable to find the profile\",\"SUBTITLE\":\"test\",\"selfIntroduction\":\"Self Introduction\",\"name\":\"Name\",\"hobby\":\"Hobby\",\"area\":\"Places I've lived\",\"club\":\"Club\",\"partTimeJob\":\"Part-time Job\",\"MainTopic\":\"Why I love Sauna\",\"Reason1\":\"Relieves fatigue by improving oxygen intake\",\"Reason1_1\":\"Sauna bathing increases blood flow nearly twice as much as at rest. As a result, oxygen intake increases, muscle fatigue substances are secreted, physical fatigue is recovered, energy is reproduced, and the effect of fatigue recovery is enhanced.\",\"Reason2\":\"It's time to face your body\",\"Reason2_2\":\"More and more people are having difficulty understanding their senses, called alexithymia. Instead of setting the time by looking at the clock or entering at the time told by others, you can regain the sense of being in touch with yourself by judging by your heart rate.\"}}"));}}),
-
-};
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pwB9zbu26JCG5eBI4AfbnNwkG8DVt5dH2NRaRu0B2/E=\"\n}"
