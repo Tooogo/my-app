220686272de1ee562101e232c95a29e179c4ebba
Togo <togo@hasrec.com>
2025-07-03 08:39:24 +0900

    modify familiy edit page based on the vercel error log

--- src/app/[locale]/family/[id]/edit/page.tsx
+++ src/app/[locale]/family/[id]/edit/page.tsx
@@ -1,19 +1,21 @@
-
 import { getProfileById } from '@/app/services';
 import ParentComponent from '@/app/components/ParentComponent';
 
-export default async function HomePage({ params }: { params: { id: string } }) {
-  const { id } = await params;
-  const profile = await getProfileById(id);
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
+export default async function HomePage({ params }: PageProps) {
+  const { id } = params;
+  const profile = await getProfileById(id);
 
   return (
     <div>
       <h1>User Profile</h1>
       {profile ? (
-        <ParentComponent
-          userData={profile}
-        />
+        <ParentComponent userData={profile} />
       ) : (
         <div>Profile not found</div>
       )}
