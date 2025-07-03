5c3b5c2a9d19646390caf6c4159511aefafc953b
Togo <togo@hasrec.com>
2025-07-03 09:30:52 +0900

    modified vercel error

--- src/app/[locale]/family/[id]/edit/page.tsx
+++ src/app/[locale]/family/[id]/edit/page.tsx
@@ -1,24 +1,16 @@
 import { getProfileById } from '@/app/services';
 import ParentComponent from '@/app/components/ParentComponent';
 
-export type PageProps = {
-  params: {
-    id: string;
-  };
-}
+// src/app/[locale]/family/[id]/edit/page.tsx
 
-export default async function HomePage({ params }: PageProps) {
+export default async function Page({ params }: { params: { id: string; locale: string } }) {
   const { id } = params;
   const profile = await getProfileById(id);
 
   return (
     <div>
       <h1>User Profile</h1>
-      {profile ? (
-        <ParentComponent userData={profile} />
-      ) : (
-        <div>Profile not found</div>
-      )}
+      {profile ? <ParentComponent userData={profile} /> : <div>Profile not found</div>}
     </div>
   );
 }
